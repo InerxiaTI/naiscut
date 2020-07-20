bb8037379b7077ccab17e0e20d6d02d40e58c28a
Juan Guillermo Hernandez Alarcon <juanguillermoalarcon@gmail.com>
2020-07-19 19:54:53 -0500

    primer commit

---
+++ src/main/java/com/inerxia/naiscut/NaiscutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inerxia.naiscut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaiscutBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaiscutBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inerxia/naiscut/NaiscutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inerxia.naiscut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiscutBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
